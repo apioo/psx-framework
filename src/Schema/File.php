a3201e53b60292baec20d8ea801daf1401163032
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-10-14 14:42:25 +0200

    add file upload schema

---
+++ src/Schema/File.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Framework\Schema;
+
+use PSX\Schema\Builder;
+use PSX\Schema\SchemaInterface;
+
+/**
+ * File upload
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class File implements SchemaInterface
+{
+    protected $definition;
+
+    public function __construct()
+    {
+        $builder = new Builder('file');
+        $builder->setDescription('File upload provided through a multipart/form-data post');
+        $builder->string('name');
+        $builder->string('type');
+        $builder->integer('size');
+        $builder->string('tmp_name');
+        $builder->integer('error');
+
+        $this->definition = $builder->getProperty();
+    }
+
+    public function getDefinition()
+    {
+        return $this->definition;
+    }
+}
