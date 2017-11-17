822c9f392b8dce9e4cdae52a3d200b9a4f2d9bc3
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-11-17 23:38:47 +0100

    fix test

--- tests/Filter/Oauth2AuthenticationTest.php
+++ tests/Filter/Oauth2AuthenticationTest.php
@@ -66,7 +66,7 @@ class Oauth2AuthenticationTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException \PSX\Http\Exception\BadRequestException
+     * @expectedException \PSX\Http\Exception\UnauthorizedException
      */
     public function testFailure()
     {
