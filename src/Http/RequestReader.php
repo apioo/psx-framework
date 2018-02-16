bf698e9b7e6019645fdf61339a2aeb4a4f9b1f05
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-02-16 22:49:53 +0100

    add request reader and response writer

---
+++ src/Http/RequestReader.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Framework\Http;
+
+use PSX\Data\Payload;
+use PSX\Data\Processor;
+use PSX\Http\RequestInterface;
+use PSX\Schema\Validation\ValidatorInterface;
+use PSX\Schema\Visitor\TypeVisitor;
+
+/**
+ * RequestReader
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class RequestReader
+{
+    /**
+     * @var \PSX\Data\Processor
+     */
+    protected $processor;
+
+    /**
+     * @param \PSX\Data\Processor $processor
+     */
+    public function __construct(Processor $processor)
+    {
+        $this->processor = $processor;
+    }
+
+    /**
+     * Returns the result of the reader for the request
+     *
+     * @param \PSX\Http\RequestInterface $request
+     * @param string $readerType
+     * @return mixed
+     */
+    public function getBody(RequestInterface $request, $readerType = null)
+    {
+        $data    = (string) $request->getBody();
+        $payload = Payload::create($data, $request->getHeader('Content-Type'))
+            ->setRwType($readerType);
+
+        return $this->processor->parse($payload);
+    }
+
+    /**
+     * @param \PSX\Http\RequestInterface $request
+     * @param string $schema
+     * @param \PSX\Schema\Validation\ValidatorInterface $validator
+     * @param string $readerType
+     * @return mixed
+     */
+    public function getBodyAs(RequestInterface $request, $schema, ValidatorInterface $validator = null, $readerType = null)
+    {
+        $data    = (string) $request->getBody();
+        $payload = Payload::create($data, $request->getHeader('Content-Type'))
+            ->setRwType($readerType);
+
+        return $this->processor->read($schema, $payload, new TypeVisitor($validator));
+    }
+}
---
+++ src/Http/ResponseWriter.php
@@ -0,0 +1,146 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Framework\Http;
+
+use PSX\Data\Payload;
+use PSX\Data\Processor;
+use PSX\Http\ResponseInterface;
+use PSX\Http\Stream\FileStream;
+use PSX\Http\StreamInterface;
+
+/**
+ * ResponseWriter
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class ResponseWriter
+{
+    /**
+     * @var \PSX\Data\Processor
+     */
+    protected $processor;
+
+    /**
+     * @param \PSX\Data\Processor $processor
+     */
+    public function __construct(Processor $processor)
+    {
+        $this->processor = $processor;
+    }
+
+    /**
+     * Method to set a response body
+     *
+     * @param \PSX\Http\ResponseInterface $response
+     * @param mixed $data
+     * @param \PSX\Framework\Http\WriterOptions $options
+     */
+    public function setBody(ResponseInterface $response, $data, WriterOptions $options = null)
+    {
+        if ($data instanceof \DOMDocument) {
+            $data = new Body\Xml($data);
+        } elseif ($data instanceof \SimpleXMLElement) {
+            $data = new Body\Xml($data);
+        } elseif ($data instanceof StreamInterface) {
+            if ($data instanceof FileStream) {
+                trigger_error('Use of the FileStream is deprecated please use the PSX\Framework\Http\Body\File wrapper', E_USER_DEPRECATED);
+
+                $response->setHeader('Content-Type', $data->getContentType());
+                $response->setHeader('Content-Disposition', 'attachment; filename="' . addcslashes($data->getFileName(), '"') . '"');
+
+                $data = new Body\Body($data->getContents());
+            } else {
+                $data = new Body\Stream($data);
+            }
+        } elseif (is_string($data)) {
+            $data = new Body\Body($data);
+        }
+
+        if ($data instanceof Body\BodyInterface) {
+            $data->writeTo($response);
+        } else {
+            $this->setResponse($response, $data, $options ?? new WriterOptions());
+        }
+    }
+
+    /**
+     * Writes the $record with the writer $writerType or depending on the get
+     * parameter format or of the mime type of the Accept header
+     *
+     * @param \PSX\Http\ResponseInterface $response
+     * @param mixed $data
+     * @param \PSX\Framework\Http\WriterOptions $options
+     * @return void
+     */
+    private function setResponse(ResponseInterface $response, $data, WriterOptions $options)
+    {
+        $format     = $options->getFormat();
+        $writerType = $options->getWriterType();
+
+        if (!empty($format) && $writerType === null) {
+            $writerType = $this->processor->getConfiguration()->getWriterFactory()->getWriterClassNameByFormat($format);
+        }
+
+        $supported = $options->getSupportedWriter();
+        $writer    = $this->processor->getWriter($options->getContentType(), $writerType, $supported);
+
+        // set writer specific settings
+        $callback = $options->getWriterCallback();
+        if ($callback instanceof \Closure) {
+            $callback($writer);
+        }
+
+        // write the response
+        $payload = Payload::create($data, $options->getContentType())
+            ->setRwType($writerType);
+
+        if (!empty($supported)) {
+            $payload->setRwSupported($supported);
+        }
+
+        $result = $this->processor->write($payload);
+
+        // the response may have multiple presentations based on the Accept
+        // header field
+        if (!$response->hasHeader('Vary')) {
+            $response->setHeader('Vary', 'Accept');
+        }
+
+        // set content type header if not available
+        if (!$response->hasHeader('Content-Type')) {
+            $contentType = $writer->getContentType();
+
+            if ($contentType !== null) {
+                $response->setHeader('Content-Type', $contentType);
+            }
+        }
+
+        // for head requests set content length and remove body
+        if ($options->getRequestMethod() == 'HEAD') {
+            $response->setHeader('Content-Length', mb_strlen($result));
+            $result = '';
+        }
+
+        $response->getBody()->write($result);
+    }
+}
