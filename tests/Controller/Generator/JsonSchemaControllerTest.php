49f3cf08814f278f269cc48c2c1dc8d47a1036c4
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-09-03 13:46:21 +0200

    add jsonschema controller

---
+++ tests/Controller/Generator/JsonSchemaControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2018 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Framework\Tests\Controller\Generator;
+
+use PSX\Framework\Controller\Generator\JsonSchemaController;
+use PSX\Framework\Test\ControllerTestCase;
+use PSX\Framework\Tests\Controller\Foo\Application\TestSchemaApiController;
+use PSX\Framework\Tests\Controller\Foo\Application\TestSchemaApiV2Controller;
+
+/**
+ * JsonSchemaControllerTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class JsonSchemaControllerTest extends ControllerTestCase
+{
+    public function testIndex()
+    {
+        $response = $this->sendRequest('/jsonschema/1/api', 'GET', ['Accept' => 'application/json']);
+        $json     = (string) $response->getBody();
+        $expect   = file_get_contents(__DIR__ . '/resource/jsonschema.json');
+
+        $this->assertEquals(null, $response->getStatusCode(), $json);
+        $this->assertEquals('application/json', $response->getHeader('Content-Type'), $json);
+        $this->assertJsonStringEqualsJsonString($expect, $json, $json);
+    }
+
+    public function testCollection()
+    {
+        $response = $this->sendRequest('/jsonschema/1/*', 'GET', ['Accept' => 'application/json']);
+        $json     = (string) $response->getBody();
+
+        $this->assertEquals(500, $response->getStatusCode(), $json);
+        $this->assertEquals('application/json', $response->getHeader('Content-Type'), $json);
+    }
+
+    protected function getPaths()
+    {
+        return array(
+            [['GET'], '/jsonschema/:version/*path', JsonSchemaController::class],
+            [['ANY'], '/api', TestSchemaApiController::class],
+            [['ANY'], '/endpoint', TestSchemaApiV2Controller::class],
+        );
+    }
+}
