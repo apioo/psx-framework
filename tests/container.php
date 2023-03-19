237d1e70c1c2c334628c90cf8a0de49b0546659e
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-03-19 22:49:09 +0100

    rename folder

--- tests/container.php
+++ tests/container.php
@@ -2,7 +2,7 @@
 
 return \PSX\Framework\Dependency\ContainerBuilder::build(
     __DIR__,
-    __DIR__ . '/../config/container.php',
+    __DIR__ . '/../resources/container.php',
     __DIR__ . '/test_container.php',
     __DIR__ . '/../app/container.php',
 );
