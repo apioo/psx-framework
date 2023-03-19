d2b42287113d7aa7d4cee271507f81971427c0a2
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-03-19 11:44:25 +0100

    add symfony debug commands

---
+++ src/Console/DebugAutowiringCommand.php
@@ -0,0 +1,156 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PSX\Framework\Console;
+
+use PSX\Framework\Console\Descriptor\Descriptor;
+use PSX\Framework\Dependency\ContainerBuilder;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Completion\CompletionInput;
+use Symfony\Component\Console\Completion\CompletionSuggestions;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * A console command for autowiring information.
+ *
+ * @author Ryan Weaver <ryan@knpuniversity.com>
+ */
+#[AsCommand(name: 'debug:autowiring', description: 'List classes/interfaces you can use for autowiring')]
+class DebugAutowiringCommand extends DebugContainerCommand
+{
+    protected function configure(): void
+    {
+        $this
+            ->setName('debug:autowiring')
+            ->setDefinition([
+                new InputArgument('search', InputArgument::OPTIONAL, 'A search filter'),
+                new InputOption('all', null, InputOption::VALUE_NONE, 'Show also services that are not aliased'),
+            ])
+            ->setHelp(<<<'EOF'
+The <info>%command.name%</info> command displays the classes and interfaces that
+you can use as type-hints for autowiring:
+
+  <info>php %command.full_name%</info>
+
+You can also pass a search term to filter the list:
+
+  <info>php %command.full_name% log</info>
+
+EOF
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $errorIo = $io->getErrorStyle();
+
+        $builder = ContainerBuilder::getContainerBuilder(
+            $this->container->getParameter('psx_app_dir'),
+            $this->container->getParameter('psx_container_files')
+        );
+
+        $serviceIds = $builder->getServiceIds();
+        $serviceIds = array_filter($serviceIds, $this->filterToServiceTypes(...));
+
+        if ($search = $input->getArgument('search')) {
+            $searchNormalized = preg_replace('/[^a-zA-Z0-9\x7f-\xff $]++/', '', $search);
+
+            $serviceIds = array_filter($serviceIds, fn ($serviceId) => false !== stripos(str_replace('\\', '', $serviceId), $searchNormalized) && !str_starts_with($serviceId, '.'));
+
+            if (!$serviceIds) {
+                $errorIo->error(sprintf('No autowirable classes or interfaces found matching "%s"', $search));
+
+                return 1;
+            }
+        }
+
+        uasort($serviceIds, 'strnatcmp');
+
+        $io->title('Autowirable Types');
+        $io->text('The following classes & interfaces can be used as type-hints when autowiring:');
+        if ($search) {
+            $io->text(sprintf('(only showing classes/interfaces matching <comment>%s</comment>)', $search));
+        }
+        $hasAlias = [];
+        $all = $input->getOption('all');
+        $previousId = '-';
+        $serviceIdsNb = 0;
+        foreach ($serviceIds as $serviceId) {
+            $text = [];
+            $resolvedServiceId = $serviceId;
+            if (!str_starts_with($serviceId, $previousId)) {
+                $text[] = '';
+                if ('' !== $description = Descriptor::getClassDescription($serviceId, $resolvedServiceId)) {
+                    if (isset($hasAlias[$serviceId])) {
+                        continue;
+                    }
+                    $text[] = $description;
+                }
+                $previousId = $serviceId.' $';
+            }
+
+            $serviceLine = sprintf('<fg=yellow>%s</>', $serviceId);
+
+            if ($builder->hasAlias($serviceId)) {
+                $hasAlias[$serviceId] = true;
+                $serviceAlias = $builder->getAlias($serviceId);
+
+                if ($builder->hasDefinition($serviceAlias) && $decorated = $builder->getDefinition($serviceAlias)->getTag('container.decorator')) {
+                    $serviceLine .= ' <fg=cyan>('.$decorated[0]['id'].')</>';
+                } else {
+                    $serviceLine .= ' <fg=cyan>('.$serviceAlias.')</>';
+                }
+
+                if ($serviceAlias->isDeprecated()) {
+                    $serviceLine .= ' - <fg=magenta>deprecated</>';
+                }
+            } elseif (!$all) {
+                ++$serviceIdsNb;
+                continue;
+            } elseif ($builder->getDefinition($serviceId)->isDeprecated()) {
+                $serviceLine .= ' - <fg=magenta>deprecated</>';
+            }
+            $text[] = $serviceLine;
+            $io->text($text);
+        }
+
+        $io->newLine();
+
+        if (0 < $serviceIdsNb) {
+            $io->text(sprintf('%s more concrete service%s would be displayed when adding the "--all" option.', $serviceIdsNb, $serviceIdsNb > 1 ? 's' : ''));
+        }
+        if ($all) {
+            $io->text('Pro-tip: use interfaces in your type-hints instead of classes to benefit from the dependency inversion principle.');
+        }
+
+        $io->newLine();
+
+        return 0;
+    }
+
+    public function complete(CompletionInput $input, CompletionSuggestions $suggestions): void
+    {
+        if ($input->mustSuggestArgumentValuesFor('search')) {
+            $builder = ContainerBuilder::getContainerBuilder(
+                $this->container->getParameter('psx_app_dir'),
+                $this->container->getParameter('psx_container_files')
+            );
+
+            $suggestions->suggestValues(array_filter($builder->getServiceIds(), $this->filterToServiceTypes(...)));
+        }
+    }
+}
---
+++ tests/Console/DebugAutowiringCommandTest.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * PSX is an open source PHP framework to develop RESTful APIs.
+ * For the current version and information visit <https://phpsx.org>
+ *
+ * Copyright 2010-2022 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Framework\Tests\Console;
+
+use PSX\Framework\Test\ControllerTestCase;
+use PSX\Framework\Test\Environment;
+use PSX\Framework\Tests\Controller\Foo\Application\TestApiController;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * DebugAutowiringCommandTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class DebugAutowiringCommandTest extends ControllerTestCase
+{
+    public function testCommand()
+    {
+        $command = Environment::getService(Application::class)->find('debug:autowiring');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $actual = trim($commandTester->getDisplay());
+        $expect = trim(file_get_contents(__DIR__ . '/output/debug_autowiring.txt'));
+
+        $this->assertEquals($expect, $actual, $actual);
+    }
+}
