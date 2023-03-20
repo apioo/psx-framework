0d625357b7ae74a6c889c724fa7520b01f9eda31
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-03-20 23:00:50 +0100

    add model and sdk command

---
+++ src/Console/ModelCommand.php
@@ -0,0 +1,103 @@
+<?php
+/*
+ * PSX is an open source PHP framework to develop RESTful APIs.
+ * For the current version and information visit <https://phpsx.org>
+ *
+ * Copyright 2010-2022 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Framework\Console;
+
+use PSX\Framework\Dependency\DirectoryInterface;
+use PSX\Schema\Generator\Code\Chunks;
+use PSX\Schema\Generator\FileAwareInterface;
+use PSX\Schema\GeneratorFactory;
+use PSX\Schema\SchemaManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * ModelCommand
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class ModelCommand extends Command
+{
+    private DirectoryInterface $directory;
+    private SchemaManagerInterface $schemaManager;
+
+    public function __construct(DirectoryInterface $directory, SchemaManagerInterface $schemaManager)
+    {
+        parent::__construct();
+
+        $this->directory = $directory;
+        $this->schemaManager = $schemaManager;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('generate:model')
+            ->setDescription('Generates model classes based on a TypeSchema specification');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $appDir = $this->directory->getAppDir();
+        $srcDir = $this->directory->getSrcDir();
+
+        $source = $appDir . '/resources/typeschema.json';
+        $target = $srcDir . '/Model';
+        $format = 'php';
+        $config = 'namespace=App\Model';
+
+        if (!is_file($source)) {
+            throw new \RuntimeException('TypeSchema file does not exist at resources/typeschema.json, please create the file in order to generate the models, more information about TypeSchema at: typeschema.org');
+        }
+
+        if (!is_dir($target)) {
+            throw new \RuntimeException('The folder src/Model does not exist, please create it in order to generate the models');
+        }
+
+        $count = $this->generate($source, $target, $format, $config);
+
+        $output->writeln('Generated ' . $count . ' files at ' . $target);
+
+        return 0;
+    }
+
+    private function generate(string $source, string $target, string $format, string $config): int
+    {
+        $schema = $this->schemaManager->getSchema($source);
+
+        $generator = (new GeneratorFactory())->getGenerator($format, $config);
+        $response  = $generator->generate($schema);
+
+        if ($generator instanceof FileAwareInterface && $response instanceof Chunks) {
+            $count = 0;
+            foreach ($response->getChunks() as $file => $code) {
+                file_put_contents($target . '/' . $generator->getFileName($file), $generator->getFileContent($code));
+                $count++;
+            }
+
+            return $count;
+        } else {
+            throw new \RuntimeException('The configured generator cant produce files');
+        }
+    }
+}
---
+++ src/Console/SdkCommand.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ * PSX is an open source PHP framework to develop RESTful APIs.
+ * For the current version and information visit <https://phpsx.org>
+ *
+ * Copyright 2010-2022 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Framework\Console;
+
+use PSX\Api\GeneratorFactory;
+use PSX\Api\GeneratorFactoryInterface;
+use PSX\Api\Scanner\FilterFactoryInterface;
+use PSX\Api\ScannerInterface;
+use PSX\Framework\Dependency\DirectoryInterface;
+use PSX\Schema\Generator\Code\Chunks;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * SdkCommand
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class SdkCommand extends Command
+{
+    private DirectoryInterface $directory;
+    private ScannerInterface $scanner;
+    private GeneratorFactoryInterface $factory;
+    private ?FilterFactoryInterface $filterFactory;
+
+    public function __construct(DirectoryInterface $directory, ScannerInterface $scanner, GeneratorFactoryInterface $factory, FilterFactoryInterface $filterFactory)
+    {
+        parent::__construct();
+
+        $this->directory = $directory;
+        $this->scanner = $scanner;
+        $this->factory = $factory;
+        $this->filterFactory = $filterFactory;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('generate:sdk')
+            ->setDescription('Generates a client SDK')
+            ->addArgument('format', InputArgument::OPTIONAL, 'The target format of the SDK', GeneratorFactoryInterface::CLIENT_TYPESCRIPT)
+            ->addOption('connection', 'c', InputOption::VALUE_REQUIRED, 'The connection which is used', 'System')
+            ->addOption('namespace', 's', InputOption::VALUE_REQUIRED, 'A namespace which is used', null)
+            ->addOption('filter', 'e', InputOption::VALUE_REQUIRED, 'Optional a filter which is used', 'default');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $appDir = $this->directory->getAppDir();
+        $dir = $appDir . '/output';
+        if (!is_dir($dir)) {
+            throw new \RuntimeException('The folder output/ does not exist, please create it in order to generate the SDK');
+        }
+
+        $format = $input->getArgument('format') ?? GeneratorFactoryInterface::CLIENT_TYPESCRIPT;
+        if (!is_string($format) || !in_array($format, GeneratorFactory::getPossibleTypes())) {
+            throw new \InvalidArgumentException('Provided an invalid format, possible values are: ' . implode(', ', GeneratorFactory::getPossibleTypes()));
+        }
+
+        $config = $this->getConfig($input);
+        $filter = null;
+        $filterName = $input->getOption('filter');
+        if (!empty($filterName) && is_string($filterName)) {
+            $filter = $this->filterFactory?->getFilter($filterName);
+            if ($filter === null) {
+                throw new \RuntimeException('Provided an invalid filter name');
+            }
+        }
+
+        $generator = $this->factory->getGenerator($format, $config);
+        $extension = $this->factory->getFileExtension($format, $config);
+
+        $output->writeln('Generating ...');
+
+        $content = $generator->generate($this->scanner->generate($filter));
+
+        if ($content instanceof Chunks) {
+            if (!empty($filterName)) {
+                $file = 'sdk-' . $format .  '-' . $filterName . '.zip';
+            } else {
+                $file = 'sdk-' . $format .  '.zip';
+            }
+
+            $content->writeTo($dir . '/' . $file);
+        } else {
+            if (!empty($filterName)) {
+                $file = 'output-' . $format . '-' . $filterName . '.' . $extension;
+            } else {
+                $file = 'output-' . $format . '.' . $extension;
+            }
+
+            file_put_contents($dir . '/' . $file, $content);
+        }
+
+        $output->writeln('Successful!');
+
+        return 0;
+    }
+
+    private function getConfig(InputInterface $input): ?string
+    {
+        $namespace = $input->getOption('namespace');
+        $options = [];
+        if (!empty($namespace)) {
+            $options['namespace'] = $namespace;
+        }
+
+        if (!empty($options)) {
+            return http_build_query($options);
+        } else {
+            return null;
+        }
+    }
+}
